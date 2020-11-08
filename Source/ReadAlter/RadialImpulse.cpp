29d1b3d79427a707629fe6a59bd8a3e1987425fe
Twentyfortyeight <daccne@gmail.com>
2020-11-08 16:02:06 +0300

    Add Radial Impulse

--- Source/ReadAlter/RadialImpulse.cpp
+++ Source/ReadAlter/RadialImpulse.cpp
@@ -2,6 +2,13 @@
 
 
 #include "RadialImpulse.h"
+#include "DrawDebugHelpers.h"
+#include "CollisionShape.h"
+#include "Runtime/Engine/Public/WorldCollision.h"
+#include "Engine/World.h"
+#include "Math/Quat.h"
+#include "Components/PrimitiveComponent.h" 
+
 
 // Sets default values
 ARadialImpulse::ARadialImpulse()
@@ -15,7 +22,24 @@ ARadialImpulse::ARadialImpulse()
 void ARadialImpulse::BeginPlay()
 {
 	Super::BeginPlay();
+	TArray<FHitResult> OutHits;
+	FVector MyLocation = GetActorLocation();
+	FCollisionShape MyCollsionSphere = FCollisionShape::MakeSphere(500.f);
+	DrawDebugSphere(GetWorld(), MyLocation, MyCollsionSphere.GetSphereRadius(), 50, FColor::Emerald, true);
 	
+	bool ItsHit = GetWorld()->SweepMultiByChannel(OutHits, MyLocation, MyLocation, FQuat::Identity, ECC_WorldStatic, MyCollsionSphere);
+	if (ItsHit)
+	{
+		for (auto&Hit:OutHits)
+		{
+			UStaticMeshComponent* MeshComp = Cast<UStaticMeshComponent>((Hit.GetActor())->GetRootComponent());
+			
+			if (MeshComp)
+			{
+				MeshComp->AddRadialImpulse(MyLocation, 500.f, 2000.f, ERadialImpulseFalloff::RIF_Constant, true);
+			}
+		}
+	}
 }
 
 // Called every frame
